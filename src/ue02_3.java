2c9f38ade1279f9e980f9d624c8b2d8c4df4b01e
Simon B <s2410238090@fh-ooe.at>
2024-10-28 20:22:08 +0100

    some comments deleted

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -30,8 +30,3 @@ public class ue02_3 {
         ;
     }
 }
-
-/*In.readdouble
-zuerst abfragen ob es in der grünen fläche ist (nicht darüber hinaus)
-dann checken ob größer oder kleiner als radius
-*/
